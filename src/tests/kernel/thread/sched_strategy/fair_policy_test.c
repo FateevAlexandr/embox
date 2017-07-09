d68916d8ce45180df95c20ad5f148394c90c5a4e
Anton Kozlov <drakon.mega@gmail.com>
2017-07-09 11:28:42 +0300

    Change fairness check (2:1 expected)

--- src/tests/kernel/thread/sched_strategy/fair_policy_test.c
+++ src/tests/kernel/thread/sched_strategy/fair_policy_test.c
@@ -20,7 +20,7 @@ static void *func(void *prt);
 TEST_CASE("scheduling_with_two_threads") {
 	flag = true;
 	void *total_t1, *total_t2;
-	int prt1 = 0, prt2 = 17;
+	int prt1 = 0, prt2 = 17; /* FIXME */
 	pthread_t t1, t2;
 
 	test_assert_zero(pthread_create(&t1, 0, func, &prt1));
@@ -32,7 +32,8 @@ TEST_CASE("scheduling_with_two_threads") {
 	test_assert_zero(pthread_join(t1, &total_t1));
 	test_assert_zero(pthread_join(t2, &total_t2));
 
-	test_assert_true((int)total_t1/(int)total_t2 > prt2 - prt1);
+	const double ratio = (double)(intptr_t)total_t1 / (double)(intptr_t)total_t2;
+	test_assert_true(1.9 <= ratio && ratio < 2.1);
 }
 
 static void *func(void *prt) {
